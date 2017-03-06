7da225da5f493f98b9a481231ca511adf596fd62
nassermaronie <maronienasser@gmail.com>
2017-03-06 12:25:45 +0700

    initial commit

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+function create(htmlStr) {
+    var frag = document.createDocumentFragment(),
+        temp = document.createElement('div');
+    temp.innerHTML = htmlStr;
+    while (temp.firstChild) {
+        frag.appendChild(temp.firstChild);
+    }
+    return frag;
+}
+
+var fragment = create('<h1>Hello World!</h1><p>What a wonderful world !</p>');
+document.getElementById('root').appendChild(fragment);
